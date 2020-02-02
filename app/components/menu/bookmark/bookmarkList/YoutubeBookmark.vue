c1377a40bc4b292be9730d8962cfbf09ee2b2a7f
남오승 <developernam92@gmail.com>
2020-02-02 19:56:46 +0900

    디자인 적용 추가 유튜브 북마크 리스트 320. 360 .420 .480

--- app/components/menu/bookmark/bookmarkList/YoutubeBookmark.vue
+++ app/components/menu/bookmark/bookmarkList/YoutubeBookmark.vue
@@ -1,44 +1,58 @@
 <template lang="html">
- <StackLayout backgroundColor="#d3d3d3">
-  <StackLayout width="95%" marginTop="10"height="96%" backgroundColor="white">
-   <ListView for="youtubeBookmark in youtubeBookmarkList"
-             style="height:100%" @itemTap="goYoutube">
+   <ListView for="youtubeBookmark in youtubeBookmarkList" separatorColor="transparent"
+             style="height:100%" @itemTap="goYoutube" >
     <v-template>
-     <StackLayout width="100%" padding="0" orientation="horizontal" >
-      <StackLayout paddingRight="-15">
-       <Image :src="youtubeBookmark.thumbnail_url" class="thumb img-thumbnail" width="40%" height="100" />
+     <StackLayout class="youtubeMarkMain" >
+      <StackLayout orientation="horizontal" class="youtubeMarkWrap" v-shadow="{ elevation: 2,shape:'RECTANGLE', bgcolor: 'white', cornerRadius: 10 }">
+       <StackLayout class="youtubeImageWrap">
+        <Image :src="youtubeBookmark.thumbnail" class="youtubeImage"/>
+       </StackLayout>
+       <StackLayout class="youtubeRightWrap">
+        <StackLayout orientation="horizontal" class="youtubeTitleWrap">
+         <Label :text="youtubeBookmark.title" class="youtubeTitle" textWrap="true"/>
+        </StackLayout>
+        <StackLayout orientation="horizontal" class="youtubeDateWrap">
+         <Label :text="youtubeBookmark.date" class="youtubeDate"  />
+        </StackLayout>
+       </StackLayout>
       </StackLayout>
-      <StackLayout width="60%" paddingLeft="0">
-       <StackLayout orientation="horizontal" style="text-align: center"  padding="0">
-        <Label :text="'@ ' +youtubeBookmark.place_name" fontSize="20" width="100%" style="text-align: left" />
+       <StackLayout class="markUnderline">
        </StackLayout>
-       <StackLayout orientation="horizontal" style="text-align: center"  padding="0">
-        <Label :text="youtubeBookmark.title" fontSize="16" width="100%" style="text-align: left" />
-       </StackLayout>
-       <StackLayout orientation="horizontal" style="text-align: center"  padding="0">
-        <Label :text="youtubeBookmark.description" fontSize="13" width="100%" style="text-align: left" />
-       </StackLayout>
-      </StackLayout>
      </StackLayout>
     </v-template>
    </ListView>
-  </StackLayout>
- </StackLayout>
 </template>
 
 <script>
  import axios from 'axios';
  import YBookmarkDetil from "./bookmarkDetail/YBookmarkDetil";
+
+ import '~/Resources/css/menu/bookmark/bookmarkList/YoutubeBookmark/youtubeBookmark_320.scss';
+ import '~/Resources/css/menu/bookmark/bookmarkList/YoutubeBookmark/youtubeBookmark_360.scss';
+ import '~/Resources/css/menu/bookmark/bookmarkList/YoutubeBookmark/youtubeBookmark_420.scss';
+ import '~/Resources/css/menu/bookmark/bookmarkList/YoutubeBookmark/youtubeBookmark_480.scss';
+
     export default {
         name:"YoutubeBookmark",
        data(){
         return {
-         youtubeBookmarkList:[]
+         youtubeBookmarkList:[
+          {
+           'thumbnail' : 'https://i.ytimg.com/vi/9f-bB4_SJkI/maxresdefault.jpg',
+           'title':'영상 제목 최대 세 줄까지 영상 제목 최대 세 줄까지 영상 제목 최대 세 줄까지 영상 제목 최대 세 줄까지',
+           'date':'7개월 전'
+          },
+          {
+           'thumbnail' : 'https://i.ytimg.com/vi/9f-bB4_SJkI/maxresdefault.jpg',
+           'title':'영상 제목 최대 세 줄까지 영상 제목 최대 세 줄까지 영상 제목 최대 세 줄까지 영상 제목 최대 세 줄까지',
+           'date':'7개월 전'
+          }
+         ]
         }
        },
         components: {
         },mounted() {
-         axios({
+        /* axios({
           method: 'get',
           url: 'http://api.matitzung.shop/v1/bookmarks?',
           //  url: 'http://192.168.1.85:9090/v1/places?',
@@ -51,7 +65,7 @@
           this.$data.youtubeBookmarkList = response.data.dataList;
          }, (error) => {
           console.log(error);
-         });
+         });*/
      },methods:{
       goYoutube(args){
        console.log("여기 타니?");
@@ -67,10 +81,5 @@
 </script>
 
 <style lang="scss">
-    // Start custom common variables
-      @import "~@nativescript/theme/scss/variables/orange";
-    // End custom common variables
-
-    // Custom styles
 
 </style>
