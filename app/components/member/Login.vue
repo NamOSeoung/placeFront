f1e5e803d3b488cf7ef335218547fe92ba28c4b6
남오승 <developernam92@gmail.com>
2020-02-03 14:39:12 +0900

    디자인 적용 추가 로그인페이지 320.360.420.480

--- app/components/member/Login.vue
+++ app/components/member/Login.vue
@@ -1,76 +1,129 @@
 <script src="../../app.js"></script>
 <template>
-       <Page actionBarHidden="true" backgroundImage="~/Resources/img/place/food.jpg" style="background-repeat: no-repeat;background-size : cover;" >
-           <StackLayout>
-               <StackLayout>
-<!--                   <Button text="건너뛰기" @tap="$navigateTo(detailPage)" color="white" style="font-weight:bold;border-width : 0;z-index: 0; background-color: transparent;font-family: nanumgothiccoding-bold;font-size: 17" width="25%" marginLeft="75%"/>-->
-                   <Button text="건너뛰기" @tap="$navigateTo(detailPage)" color="white" style="font-weight:bold;border-width : 0;z-index: 0; background-color: transparent;font-family: nanumgothiccoding-bold;font-size: 17" width="25%" marginLeft="75%"/>
-               </StackLayout>
-               <StackLayout style="text-align: center" marginTop="20%">
-                   <label text="MAT" color="white" style="font-family: nanumgothiccoding-bold;font-size: 50;"/>
-                   <label text="EAT JUNG" color="white" style="font-family: nanumgothiccoding-bold;font-size: 50;"/>
-               </StackLayout>
-               <StackLayout style="text-align: center">
-                   <TextField hint="이메일"
-                              [text]='name'
-                              secure="false"
-                              color="white"
-                              returnKeyType="done"
-                              (returnPress)="onReturnPress($event)"
-                              autocorrect="false"
-                              maxLength="10"
-                              style="font-family: nanumgothiccoding-bold;font-size: 20;placeholder-color:white;text-align: left;"
-                              (focus)="onFocus($event)"
-                              (blur)="onBlur($event)">
-                   </TextField>
-                   <TextField hint="비밀번호"
-                              [text]='name'
-                              secure="true"
-                              color="white"
-                              returnKeyType="done"
-                              (returnPress)="onReturnPress($event)"
-                              autocorrect="false"
-                              maxLength="10"
-                              style="font-family: nanumgothiccoding-bold;font-size: 20;placeholder-color:white;text-align: left;"
-                              (focus)="onFocus($event)"
-                              (blur)="onBlur($event)">
-                   </TextField>
-                   <Button text="로그인" backgroundColor="#d3d3d3" color="#6E6E6E" style="opacity: 0.7;font-family: nanumgothiccoding-bold;font-size: 20;"/>
-                   <StackLayout orientation="horizontal" width="90%">
-                       <StackLayout orientation="horizontal" style="width:87%">
-                           <label text="아이디찾기" color="white" style="font-family: nanumgothiccoding-bold;font-size: 10;"/>
-                           <label text=" | " color="white" style="font-family: nanumgothiccoding-bold;font-size: 10;"/>
-                           <label text="비밀번호 찾기" color="white" style="font-family: nanumgothiccoding-bold;font-size: 10;"/>
-                       </StackLayout>
-                       <StackLayout orientation="horizontal">
-                           <label text="회원가입" color="white" style="font-family: nanumgothiccoding-bold;font-size: 10;" />
-                       </StackLayout>
-                   </StackLayout>
-                   <image src="~/Resources/img/user/kakao_account_login_btn_large_narrow.png" width="90%" height="70" marginTop="10"/>
-                   <image src="~/Resources/img/user/naverLoginButton.png" width="90%" height="70" marginTop="10"/>
-               </StackLayout>
-           </StackLayout>
+       <Page actionBarHidden="true" backgroundSpanUnderStatusBar="true">
+              <ScrollView>
+              <StackLayout class="loginPageWrap" >
+                     <StackLayout class="loginScreenTopWrap"backgroundImage="~/Resources/img/login/christopher_jolly_1_ib_8243_c_u_8_q_unsplash.png">
+                            <StackLayout class="loginTitleMessageWrap" orientation="horizontal">
+                                   <label text="맛잇는 장소" class="loginTitleMessageMain" />
+                                   <label text="를" class="loginTitleMessageMain2"  />
+                            </StackLayout>
+                            <StackLayout class="loginTitleMessageSubWrap" >
+                                   <label text="찾아보세요." class="loginTitleMessageSub"  />
+                            </StackLayout>
+                            <StackLayout class="loginBrandLogoWrap" >
+                                   <label text="Brand Logo" class="loginBrandLogo"/>
+                            </StackLayout>
+                            <StackLayout orientation="horizontal" class="loginBarogoMainWrap"  @tap="$navigateTo(detailPage)">
+                                   <StackLayout  class="loginBarogoWrap">
+                                          <label text="둘러보기" class="loginBarogo"  />
+                                   </StackLayout>
+                                   <StackLayout class="loginBarogoRightWrap">
+                                          <image class="loginBarogoRight"  src="~/Resources/img/login/arrow-circle-right.png" />
+                                   </StackLayout>
+                            </StackLayout>
+                     </StackLayout>
+                     <StackLayout class="loginScreenBottomWrap" >
+                            <StackLayout class="loginEmailWrap">
+                                   <TextField
+                                              class="loginEmail"
+                                              hint="이메일"
+                                              [text]='name'
+                                              secure="false"
+                                              color="white"
+                                              returnKeyType="done"
+                                              (returnPress)="onReturnPress($event)"
+                                              autocorrect="false"
+                                              maxLength="10"
+                                              (focus)="onFocus($event)"
+                                              (blur)="onBlur($event)">
+                                   </TextField>
+                            </StackLayout>
+                            <StackLayout class="loginPasswordWrap" >
+                                   <TextField hint="비밀번호"
+                                              class="loginPassword"
+                                              [text]='name'
+                                              secure="true"
+                                              color="white"
+                                              returnKeyType="done"
+                                              (returnPress)="onReturnPress($event)"
+                                              autocorrect="false"
+                                              maxLength="10"
+                                              (focus)="onFocus($event)"
+                                              (blur)="onBlur($event)">
+                                   </TextField>
+                            </StackLayout>
+                            <StackLayout class="loginFindWrap" orientation="horizontal" >
+                                   <StackLayout class="loginIdFindWrapWrap" orientation="horizontal" >
+                                          <StackLayout class="loginIdFindWrap" @tap="$navigateTo(findUserIdPage)">
+                                                 <label class="loginIdFind" text="아이디찾기 | " />
+                                          </StackLayout>
+                                          <StackLayout class="loginPasswordFindWrap">
+                                                 <label class="loginPasswordFind" text="비밀번호찾기" />
+                                          </StackLayout>
+                                   </StackLayout>
+                                   <StackLayout class="loginButtonWrap" >
+                                          <label class="loginButton" text="로그인" />
+                                   </StackLayout>
+                            </StackLayout>
+                            <StackLayout  class="loginSocialWrap">
+                                   <StackLayout class="loginEmailJoinWrap" >
+                                          <label text="이메일 회원가입" class="loginEmailJoin"  />
+                                   </StackLayout>
+                                   <StackLayout class="loginKakaoJoinWrap" orientation="horizontal">
+                                          <StackLayout class="loginKakaoIconWrap" >
+                                                 <image class="loginKakaoIcon"  src="~/Resources/img/login/kakao-circle.png" />
+                                          </StackLayout>
+                                          <StackLayout class="loginKakaoLoginWrap">
+                                                 <label class="loginKakaoLogin" text="카카오 로그인"  />
+                                          </StackLayout>
+                                   </StackLayout>
+                                   <StackLayout class="loginNaverJoinWrap" orientation="horizontal" >
+                                          <StackLayout class="loginNaverIconWrap">
+                                                 <image class="loginNaverIcon" src="~/Resources/img/login/naver-circle.png" />
+                                          </StackLayout>
+                                          <StackLayout class="loginNaverLoginWrap" >
+                                                 <label class="loginNaverLogin" text="네이버 로그인"  style="font-family: nanumsquareroundeb" />
+                                          </StackLayout>
+                                   </StackLayout>
+                            </StackLayout>
+                     </StackLayout>
+              </StackLayout>
+              </ScrollView>
        </Page>
 </template>
 
 <script>
-    import MenuWrap from "../menu/MenuWrap";
-    export default {
-        components : {
-        },
-        data: () => {
-            return {
-                detailPage: MenuWrap
-            }
-        }
-    };
+       import MenuWrap from "../menu/MenuWrap";
+       import FindUserId from "./findUserInfo/FindUserId"
+       const platformModule = require("tns-core-modules/platform");
+
+       import platformcss from '../../platformcss';
+
+       import '~/Resources/css/member/Login/login_320.scss';
+       import '~/Resources/css/member/Login/login_360.scss';
+       import '~/Resources/css/member/Login/login_420.scss';
+       import '~/Resources/css/member/Login/login_480.scss';
+       export default {
+              components : {
+              },
+              data: () => {
+                     return {
+                            detailPage: MenuWrap,
+                            findUserIdPage : FindUserId
+                     }
+              },
+              mounted() {
+                     platformcss.sizeGroupings([480,440,400,360,320]);
+                     console.log("디피아이" + platformModule.screen.mainScreen.widthDIPs)
+                     console.log("픽셀" + platformModule.screen.mainScreen.widthPixels)
+                     console.log(`screen.mainScreen.scale ${platformModule.screen.mainScreen.scale}`);
+
+
+              }
+       };
 </script>
 
 <style scoped lang="scss">
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
 </style>
